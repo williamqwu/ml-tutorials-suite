6ed7dd79135bad0c529e2e5c2a7d82a8265a928b
William Wu <williamqwu@gmail.com>
2025-06-07 23:25:48 -0400

    [style] refine plotter

--- infra/plot.py
+++ infra/plot.py
@@ -6,8 +6,15 @@ import matplotlib.animation as animation
 from IPython.display import HTML
 from PIL import Image
 
+CFG_ML1 = {
+    "c_pos": "blue",
+    "c_neg": "blurede",
+    "c_lds": "#00B050",
+    "path_prefix": ".tmp/ml_1",
+}
+
 def __ml1_load_data(case: int, mode: str = "train"):
-    datapath = f".tmp/ml_1/data_case{case}.pt"
+    datapath = os.path.join(CFG_ML1['path_prefix'], f"data_case{case}.pt")
     data = torch.load(datapath)
     x_train, y_train = data["x_train"], data["y_train"]
     assert x_train.shape[1] == 2
@@ -16,7 +23,7 @@ def __ml1_load_data(case: int, mode: str = "train"):
     return x_np, y_np
 
 def __ml1_load_w(case: int):
-    datapath = f".tmp/ml_1/stats_case{case}.npz"
+    datapath = os.path.join(CFG_ML1['path_prefix'], f"stats_case{case}.npz")
     w = np.load(datapath)["w_hist"]
     return w
 
@@ -26,8 +33,8 @@ def ml1_show_dataset_2d(case: int):
     neg = y_np < 0
 
     plt.figure(figsize=(6,6), dpi=200)
-    plt.scatter(x_np[pos, 0], x_np[pos, 1], c='blue', label='Positive', alpha=0.6)
-    plt.scatter(x_np[neg, 0], x_np[neg, 1], c='red', label='Negative', alpha=0.6)
+    plt.scatter(x_np[pos, 0], x_np[pos, 1], c=CFG_ML1['c_pos'], label='Positive', alpha=0.6)
+    plt.scatter(x_np[neg, 0], x_np[neg, 1], c=CFG_ML1['c_neg'], label='Negative', alpha=0.6)
 
     plt.title("Visualization of Generated Dataset")
     plt.xlabel("$x_1$")
@@ -50,7 +57,7 @@ def ml1_gen_w_seq(case: int, lastepoch: int = -1):
 
     x_min, x_max = x_np[:, 0].min() - 1, x_np[:, 0].max() + 1
     y_min, y_max = x_np[:, 1].min() - 1, x_np[:, 1].max() + 1
-    print(x_min,x_max,y_min,y_max)
+    # print(x_min,x_max,y_min,y_max)
 
     for epoch in range(epochs):
         plt.figure(figsize=(6, 6),dpi=200)
@@ -58,19 +65,19 @@ def ml1_gen_w_seq(case: int, lastepoch: int = -1):
         y_seen = y_np[:epoch+1]
         pos = y_seen > 0
         neg = y_seen < 0
-        plt.scatter(x_seen[pos, 0], x_seen[pos, 1], color="blue", label="Positive", alpha=0.6)
-        plt.scatter(x_seen[neg, 0], x_seen[neg, 1], color="red", label="Negative", alpha=0.6)
+        plt.scatter(x_seen[pos, 0], x_seen[pos, 1], color=CFG_ML1['c_pos'], label="Positive", alpha=0.6)
+        plt.scatter(x_seen[neg, 0], x_seen[neg, 1], color=CFG_ML1['c_neg'], label="Negative", alpha=0.6)
 
         w = w_hist[epoch]
         x_vals = np.array([x_min, x_max])
         if abs(w[1]) < 1e-8:
-            plt.axvline(x=0, color='#00B050', linestyle='--', label="Decision boundary")
+            plt.axvline(x=0, color=CFG_ML1['c_lds'], linestyle='--', label="Decision boundary")
         else:
             slope = -w[0] / w[1]
             intercept = 0
             y_vals = slope * x_vals + intercept
-            plt.plot(x_vals, y_vals, color='#00B050', linestyle='--', label="Decision boundary")
-        plt.title(f"Epoch {epoch+1}")
+            plt.plot(x_vals, y_vals, color=CFG_ML1['c_lds'], linestyle='--', label="Decision boundary")
+        plt.title(f"Phase {epoch+1}")
         plt.xlabel("$x_1$")
         plt.ylabel("$x_2$")
         plt.legend()
@@ -80,13 +87,13 @@ def ml1_gen_w_seq(case: int, lastepoch: int = -1):
         plt.gca().set_aspect('equal', adjustable='box')
         plt.tight_layout()
 
-        outputpath = f".tmp/ml_1/fig_case{case}"
+        outputpath = os.path.join(CFG_ML1['path_prefix'], f"fig_case{case}")
         os.makedirs(outputpath, exist_ok=True)
         plt.savefig(os.path.join(outputpath, f"w_e{epoch+1:03d}.png"))
         plt.close()
 
 def ml1_animate(case: int, fps: int = 2):
-    figpath = f".tmp/ml_1/fig_case{case}"
+    figpath = os.path.join(CFG_ML1['path_prefix'], f"fig_case{case}")
     frame_files = sorted([
         os.path.join(figpath, f) for f in os.listdir(figpath)
         if f.endswith(".png")
